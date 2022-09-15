5cb6f8dbfca08427a1622c2a9c39a130c843e680
Ubuntu <ubuntu@ip-172-22-22-3.us-east-2.compute.internal>
2022-09-15 10:22:02 +0000

    release config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const CONTRACT_NAME = process.env.CONTRACT_NAME || 'manhndev.testnet'
+const CONTRACT_NAME = process.env.CONTRACT_NAME || 'vi-storage.merchi.testnet'
 
 function getConfig(env) {
   switch (env) {
