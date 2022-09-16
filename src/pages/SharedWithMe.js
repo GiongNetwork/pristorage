3a31d4f9374f447ed8285adf0bd0f08aaba68e77
haonk <haonk@foobla.com>
2022-09-16 13:28:04 +0700

    develop

--- src/pages/SharedWithMe.js
+++ src/pages/SharedWithMe.js
@@ -151,7 +151,7 @@ export default function SharedWithMe() {
     const redirectToFolder = (id) => {
         if (!id || id === rootFolder?.parent) {
             history.push(`/shared_with_me`)
-            setNewFolderId(id)
+            setNewFolderId('')
             // history.go(0)
         } else {
             history.push(`/shared_with_me?doc_id=${id}`)
@@ -159,8 +159,7 @@ export default function SharedWithMe() {
             // history.go(0)
         }
     }
-    console.log(!!getUrlParameter("doc_id"), !window.location.href.includes('shared_with_me'))
-if(!window.location.href.includes('shared_with_me')){ }
+
     const downloadFile = async (record) => {
         const {cid, encrypted_password, name, file_type} = record
         if (encrypted_password) {
