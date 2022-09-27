b5a2e3ccaae5e05957f458fa57b2daed05851ba9
namtm <namtm@foobla.com>
2022-09-27 14:06:46 +0700

    update deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 DEPLOY_SERVER=$DEPLOY_SERVER
+SERVER_FOLDER="pristorage.mymerchize.com"
 
 # Building React output
 yarn install
 yarn build:web
 
 echo "Deploying to ${DEPLOY_SERVER}"
-#sudo cp -R dist/* /var/www/pristorage.mymerchize.com/html/
+scp -r dist/* ubuntu@${DEPLOY_SERVER}:/var/www/${SERVER_FOLDER}/html/
 
 echo "Finished copying the build files"
